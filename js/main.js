84efcdd12d1f6aaeac6bca265b5aa4a3080920bf
avgvstvs <andronov.n.g@gmail.com>
2020-06-17 22:39:11 +0300

    Исправил ошибки в ДЗ в соответствии с критериями

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 'use strict';
 
-//Объявление констант
 var mapFaded = document.querySelector('.map');
 mapFaded.classList.remove('map--faded');
 
@@ -31,66 +30,67 @@ var LOCATION_Y_MIN = 130;
 var LOCATION_Y_MAX = 630;
 
 var getRandomNumberInRange = function (min, max) {
-    var rand = min + Math.random() * (max + 1 - min);
-    return Math.floor(rand);
+  var rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
 };
 
-var getRandomStringInArray = function (arr) {
-    var arr = arr[getRandomNumberInRange(0, arr.length - 1)];
-    return arr;
+
+var getRandomStringInArray = function () {
+  var arr = arr[getRandomNumberInRange(0, arr.length - 1)];
+  return arr;
 };
 
 var createApartment = function () {
   var makeAd = [];
-    for (var i = 0; i < NUMBER_PINS; i++) {
+  for (var i = 0; i < NUMBER_PINS; i++) {
 
-      var locationX = getRandomNumberInRange(LOCATION_X_MIN, LOCATION_X_MAX);
-      var locationY = getRandomNumberInRange(LOCATION_Y_MIN, LOCATION_Y_MAX);
+    var locationX = getRandomNumberInRange(LOCATION_X_MIN, LOCATION_X_MAX);
+    var locationY = getRandomNumberInRange(LOCATION_Y_MIN, LOCATION_Y_MAX);
 
-      var ad = {
-        author: {
-          avatar: AUTHOR_AVATAR + getRandomNumberInRange(1, 8) + '.png'
-        },
-        offer: {
-          title: OFFER_TITLE + getRandomNumberInRange(1, 8),
-          address: locationX + ', ' + locationY,
-          price: getRandomNumberInRange(OFFER_MIN_PRICE, OFFER_MAX_PRICE),
-          type: getRandomStringInArray(OFFER_TYPE),
-          rooms: getRandomNumberInRange(OFFER_MIN_ROOMS, OFFER_MAX_ROOMS),
-          guests: getRandomNumberInRange(OFFER_MIN_GUESTS, OFFER_MAX_GUESTS),
-          checkin: getRandomStringInArray(OFFER_CHECKIN),
-          checkout: getRandomStringInArray(OFFER_CHECKOUT),
-          features: getRandomStringInArray(OFFER_FEATURES),
-          description: OFFER_DESCRIPTION + getRandomNumberInRange(1, 8),
-          photos: OFFER_PHOTOS + getRandomNumberInRange(1, 3) + '.jpg'
-        },
-        location: {
-          x: locationX,
-          y: locationY
-        }
-      };
-      makeAd.push(ad);
-    }
-
-    return makeAd;
-  };
-
-  var getPinTemplate = function (data) {
-    var pinElement = templatePin.cloneNode(true);
-    var pinIcon = pinElement.querySelector('img');
-    pinElement.style = 'left: ' + data.location.x + 'px; top: ' + data.location.y + 'px;';
-    pinIcon.src = data.author.avatar;
-    pinIcon.alt = data.offer.title;
-
-    return pinElement;
+    var ad = {
+      author: {
+        avatar: AUTHOR_AVATAR + getRandomNumberInRange(1, 8) + '.png'
+      },
+      offer: {
+        title: OFFER_TITLE + getRandomNumberInRange(1, 8),
+        address: locationX + ', ' + locationY,
+        price: getRandomNumberInRange(OFFER_MIN_PRICE, OFFER_MAX_PRICE),
+        type: getRandomStringInArray(OFFER_TYPE),
+        rooms: getRandomNumberInRange(OFFER_MIN_ROOMS, OFFER_MAX_ROOMS),
+        guests: getRandomNumberInRange(OFFER_MIN_GUESTS, OFFER_MAX_GUESTS),
+        checkin: getRandomStringInArray(OFFER_CHECKIN),
+        checkout: getRandomStringInArray(OFFER_CHECKOUT),
+        features: getRandomStringInArray(OFFER_FEATURES),
+        description: OFFER_DESCRIPTION + getRandomNumberInRange(1, 8),
+        photos: OFFER_PHOTOS + getRandomNumberInRange(1, 3) + '.jpg'
+      },
+      location: {
+        x: locationX,
+        y: locationY
+      }
+    };
+    makeAd.push(ad);
   }
 
-  var renderPins = function (pinsData) {
-    var fragment = document.createDocumentFragment();
-    for (var i = 0; i < pinsData.length; i++) {
-      fragment.appendChild(getPinTemplate(pinsData[i]));
-    }
-    mapPins.appendChild(fragment);
-  }
+  return makeAd;
+};
 
-  renderPins(createApartment());
+var getPinTemplate = function (data) {
+  var pinElement = templatePin.cloneNode(true);
+  var pinIcon = pinElement.querySelector('img');
+  pinElement.style = 'left: ' + data.location.x + 'px; top: ' + data.location.y + 'px;';
+  pinIcon.src = data.author.avatar;
+  pinIcon.alt = data.offer.title;
+
+  return pinElement;
+};
+
+var renderPins = function (pinsData) {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < pinsData.length; i++) {
+    fragment.appendChild(getPinTemplate(pinsData[i]));
+  }
+  mapPins.appendChild(fragment);
+};
+
+renderPins(createApartment());
